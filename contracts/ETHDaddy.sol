0ae0489cb87a493a39aa83ecccf4a5034df29a8b
Shubham Pathak <95904081+Shubhh4@users.noreply.github.com>
2023-04-29 12:32:07 +0530

    Update ETHDaddy.sol

--- contracts/ETHDaddy.sol
+++ contracts/ETHDaddy.sol
@@ -19,7 +19,6 @@ contract ETHDaddy is ERC721 {
     modifier onlyOwner() {
         require(msg.sender == owner);
         _;
-        
     }
 
     constructor(string memory _name, string memory _symbol)
